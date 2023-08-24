2d5cd5ab41c8b8dd72376e69d5eae9c3c9c91bf1
“Daniel12082” <137439716+Daniel12082@users.noreply.github.com>
2023-08-24 13:25:22 -0500

    castas

--- main.js
+++ main.js
@@ -12,12 +12,11 @@ let construirEncabezado =async()=>{
 let construirCartas =async()=>{
     let peticion = await fetch(`${path}.json `);
     let res = await peticion.json();
-    let Selecion = document.querySelector("#myjsonSection");
+    let Selecion = document.querySelector("#cartas");
     Selecion.insertAdjacentHTML("beforeend",/*html*/ `
-    <h1 class="display-6 fw-normal text-body-emphasis d-flex justify-content-start">${res.section.titulo}</h1>
-        ${res.section.parrafo.map((value)=>/*html*/` 
-        <p class="fs-6 text-body-secondary d-flex justify-content-start">${value.icon}${value.text}</p>`).join(" ")}
+    
     `)
 }
 
 construirEncabezado();
+construirCartas();
