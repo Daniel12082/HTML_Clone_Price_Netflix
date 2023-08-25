d85bf87328831845be3d7fb20735080ac7926a74
Daniel12082 <137439716+Daniel12082@users.noreply.github.com>
2023-08-24 22:07:53 -0500

    Cards

--- main.js
+++ main.js
@@ -9,13 +9,30 @@ let construirEncabezado =async()=>{
         <p class="fs-6 text-body-secondary d-flex justify-content-start">${value.icon}${value.text}</p>`).join(" ")}
     `)
 }
-let construirCartas =async()=>{
-    let peticion = await fetch(`${path}.json `);
+let construirCartas = async () => {
+    let peticion = await fetch(`${path}.json`);
     let res = await peticion.json();
     let Selecion = document.querySelector("#cartas");
-    Selecion.insertAdjacentHTML("beforeend",/*html*/ `
-    
-    `)
+    Selecion.insertAdjacentHTML("beforeend", /*html*/ `
+        ${res.cartas.cardcontent.map((value) => /*html*/ `
+            <div class="col">
+                ${value.Body.Border.border_red}
+                    ${value.Body.Border.background}
+                        <h4 class="my-0 fw-normal">${value.header}</h4>
+                    </div>
+                    <div class="card-body">
+                        ${value.Body.precio}
+                        <ul class="list-unstyled mt-3 mb-4">
+                            <li>${value.Body.servicios.device}</li>
+                            <li>${value.Body.servicios.support}</li>
+                            <li>${value.Body.servicios.Help}</li>
+                        </ul>
+                        <button type="button" class=${value.Body.Border.button}>Select</button>
+                    </div>
+                </div>
+            </div>
+        `).join(" ")}
+    `);
 }
 
 construirEncabezado();
